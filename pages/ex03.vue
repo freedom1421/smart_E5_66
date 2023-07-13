43deeeb213d451537b1d3c48c17aa8be532f4324
freedom1421 <d50565@ccollege.ac.th>
2023-07-13 15:42:24 +0700

    sd

---
+++ pages/ex03.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <h1>Hello {{ name }}</h1>
+      <h1>Hello {{ upperName }}</h1>
+      <input type="text" :value="name">
+      <div>one-way<input type="text" :value="name"></div>
+      <div>two-way<input type="text" v-model="name"></div>
+      <div><button @click="onSave">SAVE</button></div>
+    </div>
+  </template>
+  <script>
+  export default {
+   data() {
+    return {
+      name: "alongkorn",
+      age: 50,
+    }
+   }, 
+   computed: {
+    upperName(){
+      return this.name.toUpperCase()
+    }
+   },
+   methods: {
+    onSave(){
+      console.log("Save data");
+      console.log(this.name)
+    }
+   },
+  }
+  </script>
