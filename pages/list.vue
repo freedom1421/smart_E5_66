49a21d06843b82e3b422e321c094d6dbc8488f13
freedom1421 <d50565@ccollege.ac.th>
2023-07-20 15:56:22 +0700

    sdss

---
+++ pages/list.vue
@@ -0,0 +1,41 @@
+<template>
+      
+    <div>
+      <h1>Student List</h1>
+      <div><button @click="onSave">list</button></div>
+  <table border="1">
+    <tr>
+      <th>username</th>
+      <th>password</th> 
+      <th>dep</th>
+    </tr>
+    <tr v-for="st in student" :key="st.user_id" >
+      <td>{{ st.username }}</td>
+      <td>{{ st.password }}</td>
+      <td>{{  st.dep }}</td>
+    </tr>
+  </table>
+    </div>
+  
+  </template>
+  <script>
+  export default {
+   data() {
+   }, 
+   computed: {
+    upperName(){
+      return this.name.toUpperCase()
+    }
+   },
+   methods: {
+    async onSave(){
+      console.log(this.name)
+      const url = 'http://localhost:7001/list';
+      const res = await fetch(url);
+      const data = await res.json()
+      this.student = data.datas
+      console.log('data=>',data.datas.firstname)
+    }
+   },
+  }
+  </script>
--- pages/register.vue
+++ pages/register.vue
@@ -1,80 +1,146 @@
 <template>
-    <v-form
-    
-      ref="form"
-      v-model="valid"
-      lazy-validation
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="username"
+      :counter="10"
+      label="UserName"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="password"
+      label="Password"
+      required
+    ></v-text-field>
+
+    <v-select
+      v-model="dep"
+      :items="items"
+      label="DEPARTMENT"
+      required
+    ></v-select>
+    <v-btn
+      color="error"
+      class="mr-4"
+      @click="reset"
     >
-      <v-text-field
-        v-model="username"
-        :counter="10"
-        label="UserName"
-        required
-      ></v-text-field>
+      Reset Form
+    </v-btn>
+    <v-btn
+      color="warning"
+      @click="doSave"
+    >
+      Save
+    </v-btn>
+  </v-form>
   
-      <v-text-field
-        v-model="password"
-        label="Password"
-        required
-      ></v-text-field>
+
+  <div class="text-center">
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+         <v-card>
+        <v-card-title class="text-h5 grey lighten-2">
+          สถานะการบันทึก
+        </v-card-title>
+
+        <v-card-text>
+           บันทึกข้อมูลสำเร็จ
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+          >
+            ตกลง
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+
+  <div class="text-center">
+    <v-dialog
+      v-model="dialog_error"
+      width="500"
+    >
+         <v-card>
+        <v-card-title class="text-h5 grey lighten-2">
+          สถานะการบันทึก
+        </v-card-title>
+
+        <v-card-text>
+           บันทึกข้อมูลไม่สำเร็จ
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="dialog_error = false"
+          >
+            ตกลง
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
   
-      <v-select
-        v-model="dep"
-        :items="items"
-        label="DEPARTMENT"
-        required
-      ></v-select>
-      <v-btn
-        color="error"
-        class="mr-4"
-        @click="reset"
-      >
-        Reset Form
-      </v-btn>
-      <v-btn
-        color="warning"
-        @click="doSave"
-      >
-        Save
-      </v-btn>
-    </v-form>
-  </template>
-  <script>
-    export default {
-      data: () => ({
-        valid: true,
-        username: '',
-        password: '',   
-        dep: null,
-        items: [
-          'IT',
-          'computer',
-          'electronic',
-          'electircal power',
-        ],
-      }),
-  
-      methods: {
-        async doSave () {
-          console.log('save data')
-          console.log(this.username)
-          console.log(this.password)
-          console.log(this.dep)
-        //   let username = this.username
-        //   let password = this.password
-        //   let dep = this.dep
-          //http://localhost/7001/insert?name=username&passwd=password&dep=dep
-            const url = 'http://localhost:7001/insert?name='+this.username+'&password='+this.password+'&dep='+this.dep;
-            const res = await fetch(url);
-            const data = await res.json()
-            console.log(data.ok)
-            if(data.ok == 1){
-                console.log('save succes')
-            }
-        },
-        reset () {
-          this.$refs.form.reset()
-        },
-       },
-    }
-  </script>
+</template>
+<script>
+  export default {
+    data: () => ({
+      valid: true,
+      username: '',
+      password: '',   
+      dep: '',
+      dialog: false,
+      dialog_error : false,
+      items: [
+        'IT',
+        'computer',
+        'electronic',
+        'electircal power',
+      ],
+    }),
+
+    methods: {
+       async doSave () {
+        console.log('save data')
+        console.log(this.username)
+        console.log(this.password)
+        console.log(this.dep)
+        
+        //http://localhost/7001/insert?name=username&passwd=password&dep=dep
+        const url = 'http://localhost:7001/insert?name='+ this.username +'&password=' + this.password + '&dep='+ this.dep ;
+        const res = await fetch(url);
+        const data = await res.json()
+        if (data.ok == 't') {
+          console.log('save success');
+         this.dialog = true
+        } else {
+          console.log('save fali');
+         this.dialog_error = true
+        }
+
+      },
+      
+      reset () {
+        this.$refs.form.reset()
+      },
+     },
+  }
+</script>
