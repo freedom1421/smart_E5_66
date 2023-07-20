68d0e0a5f7684e32a5aee8e9524017e588623de6
freedom1421 <d50565@ccollege.ac.th>
2023-07-20 09:26:13 +0700

    200766

--- pages/ex03.vue
+++ pages/ex03.vue
@@ -1,34 +1,53 @@
 <template>
-    <div>
-      <h1>Hello {{ name }}</h1>
-      <h1>Hello {{ upperName }}</h1>
-      <input type="text" :value="name">
-      <div>one-way<input type="text" :value="name"></div>
-      <div>two-way<input type="text" v-model="name"></div>
-      <div><button @click="onSave">SAVE</button></div>
-    </div>
-  </template>
-  <script>
-  export default {
-   data() {
-    return {
-      name: "alongkorn",
-      age: 50,
-    }
-   }, 
-   computed: {
-    upperName(){
-      return this.name.toUpperCase()
-    }
-   },
-   methods: {
+  <div>
+    <h1>Hello {{ name }}</h1>
+    <h1>Hello {{ upperName }}</h1>
+    <input type="text" :value="name">
+    <div>one-way<input type="text" :value="name"></div>
+    <div>two-way<input type="text" v-model="name"></div>
+    <div><button @click="onSave">SAVE</button></div>
+  </div>
+  <div>
+    <h1>Student List</h1>
+    
+<table border="1">
+  <tr>
+    <th>user_id</th>
+    <th>Fisrtname</th> 
+    <th>Email</th>
+  </tr>
+  <tr v-for="st in student" :key="st.user_id" >
+    <td>{{ st.user_id }}</td>
+    <td>{{ st.firstname }}</td>
+    <td>{{  st.email }}</td>
+  </tr>
+</table>
+  </div>
+
+</template>
+<script>
+export default {
+ data() {
+  return {
+    name: "alongkorn",
+    age: 50,
+    student: []
+  }
+ }, 
+ computed: {
+  upperName(){
+    return this.name.toUpperCase()
+  }
+ },
+ methods: {
   async onSave(){
     console.log(this.name)
     const url = 'http://localhost:7001/lists';
     const res = await fetch(url);
     const data = await res.json()
-    console.log('data=>',data)
+    this.student = data.datas
+    console.log('data=>',data.datas.firstname)
   }
  },
-  }
-  </script>
+}
+</script>
