d52993ca1f1acbe32d21e68b8ae0e3b3b4d1d6e9
freedom1421 <d50565@ccollege.ac.th>
2023-07-20 12:21:31 +0700

    sdss

--- pages/register.vue
+++ pages/register.vue
@@ -1,91 +1,112 @@
 <template>
-    <v-sheet width="300" class="mx-auto">
-  
-      <v-form ref="form">
-        <v-text-field
-          v-model="name"
-          :counter="10"
-          :rules="nameRules"
-          label="Name"
-          required
-        ></v-text-field>
-  
-        <v-select
-          v-model="select"
-          :items="items"
-          :rules="[v => !!v || 'Item is required']"
-          label="Item"
-          required
-        ></v-select>
-  
-        <v-checkbox
-          v-model="checkbox"
-          :rules="[v => !!v || 'You must agree to continue!']"
-          label="Do you agree?"
-          required
-        ></v-checkbox>
-  
-        <div class="d-flex flex-column">
-          <v-btn
-            color="success"
-            class="mt-4"
-            block
-            @click="validate"
-          >
-            Validate
-          </v-btn>
-  
-          <v-btn
-            color="error"
-            class="mt-4"
-            block
-            @click="reset"
-          >
-            Reset Form
-          </v-btn>
-  
-          <v-btn
-            color="warning"
-            class="mt-4"
-            block
-            @click="resetValidation"
-          >
-            Reset Validation
-          </v-btn>
-        </div>
-      </v-form>
-    </v-sheet>
-  </template>
-  <script>
-    export default {
-      data: () => ({
-        name: '',
-        nameRules: [
-          v => !!v || 'Name is required',
-          v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-        ],
-        select: null,
-        items: [
-          'Item 1',
-          'Item 2',
-          'Item 3',
-          'Item 4',
-        ],
-        checkbox: false,
-      }),
-  
-      methods: {
-        async validate () {
-          const { valid } = await this.$refs.form.validate()
-  
-          if (valid) alert('Form is valid')
-        },
-        reset () {
-          this.$refs.form.reset()
-        },
-        resetValidation () {
-          this.$refs.form.resetValidation()
-        },
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="username"
+      :counter="10"
+      :rules="nameRules"
+      label="Name"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="password"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+    
+    <v-text-field
+      v-model="dep"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+
+    <v-select
+      v-model="select"
+      :items="items"
+      :rules="[v => !!v || 'Item is required']"
+      label="Item"
+      required
+    ></v-select>
+
+    <v-checkbox
+      v-model="checkbox"
+      :rules="[v => !!v || 'You must agree to continue!']"
+      label="Do you agree?"
+      required
+    ></v-checkbox>
+
+    <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="validate"
+    >
+      Validate
+    </v-btn>
+
+    <v-btn
+      color="error"
+      class="mr-4"
+      @click="reset"
+    >
+      Reset Form
+    </v-btn>
+
+    <v-btn
+      color="warning"
+      @click="resetValidation"
+    >
+      Reset Validation
+    </v-btn>
+  </v-form>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      valid: true,
+      username: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+      ],
+      password: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      dep: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+      ],
+      checkbox: false,
+    }),
+
+    methods: {
+      validate () {
+        this.$refs.form.validate()
       },
-    }
-  </script>
+      reset () {
+        this.$refs.form.reset()
+      },
+      resetValidation () {
+        this.$refs.form.resetValidation()
+      },
+    },
+  }
+</script>
